8f377e50e95c120682486879104e153cb6636f16
Eric Gagne <eric.jc.gagne@gmail.com>
2019-03-10 21:14:32 -0500

    Created disc model and migration

---
+++ db/migrate/20190311021404_create_discs.rb
@@ -0,0 +1,17 @@
+class CreateDiscs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :discs do |t|
+      t.string :make
+      t.string :model
+      t.string :type
+      t.string :plastic
+      t.float :dc_flight
+      t.integer :nova_speed
+      t.integer :nova_glide
+      t.integer :nova_turn
+      t.integer :nova_fade
+
+      t.timestamps
+    end
+  end
+end
