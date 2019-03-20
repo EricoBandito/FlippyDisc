88f7539d50e7595473e067a1b9c54e4f6c8d3d62
Eric Gagne <eric.jc.gagne@gmail.com>
2019-03-20 17:06:05 -0500

    Create disc show backend and started frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'discs/index'
-  get 'discs/show'
+  resources :discs, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "discs#index"
